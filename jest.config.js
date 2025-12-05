f3d505491714d7183f5e478ea245d7b7d4ca1455
Shinji De La Kazumi <144513116+ShiinjiZX@users.noreply.github.com>
2025-12-05 12:19:06 +0900

    Upload jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/app.js',
+    '!**/node_modules/**',
+  ],
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  verbose: true
+};
